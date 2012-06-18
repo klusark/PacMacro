cc1882dbe155ea6bcd93bda18e8cea177aecd11c
Joel Teichroeb <joel@teichroeb.net>
2012-06-17 19:37:16 -0700

    Switched scripts to new system

--- scripts/ingame.js
+++ scripts/ingame.js
@@ -1,5 +1,68 @@
+var websocket;
+var offset = 80;
+
+window.onload = function() {
+	"use strict";
+	var ingame = new InGame();
+	websocket = new WebSocket("ws://127.0.0.1:37645");
+	websocket.onopen = function () { websocket.send("login;" + window.location.hash.substring(1)); };
+	websocket.onmessage = function(data) { ingame.UpdateGame(data.data); };
+};
+
+function FormattedMinutesSeconds(time) {
+	"use strict";
+	var seconds = time % 60,
+		output = Math.floor(time / 60) + ":";
+	if (seconds < 10) {
+		output += "0";
+	}
+	output += seconds;
+	return output;
+}
+
+function TileToXY(tile) {
+	"use strict";
+	var x = 0, y = 0;
+	if (tile < 80) {
+		x = tile % 16;
+		y = ((tile - x) / 16) * 128 + offset;
+		x = (x * 32) + offset;
+	} else {
+		tile -= 80;
+		y = tile % 12;
+		x = ((tile - y) / 12) * 96 + offset;
+		y = (y + Math.floor(y / 3) + 1) * 32 + offset;
+	}
+	return {x : x, y : y};
+}
+
+function XYToTile(x, y) {
+	"use strict";
+	x = Math.floor(x / 16);
+	y = Math.floor(y / 16);
+	var tile = -1;
+	if (x % 2 === 0 && y % 2 === 0) {
+		if (y % 8 === 0) {
+			tile = x / 2 + (y / 8) * 16;
+		} else if (x % 6 === 0) {
+			tile = 79 + y / 2 + (x / 6) * 12 - Math.floor(y / 8);
+		}
+	}
+	return tile;
+}
+
+function IsBeside(a, b) {
+	"use strict";
+	var oa = TileToXY(a),
+		ob = TileToXY(b);
+	if ((oa.x === ob.x && (oa.y + 32 === ob.y || oa.y - 32 === ob.y)) || (oa.y === ob.y && (oa.x + 32 === ob.x || oa.x - 32 === ob.x))) {
+		return true;
+	}
+	return false;
+}
 
 function StaticImage(img, x, y, w, h, ox, oy) {
+	"use strict";
 	if (!ox) {
 		ox = 0;
 	}
@@ -8,17 +71,30 @@ function StaticImage(img, x, y, w, h, ox, oy) {
 	}
 
 	this.Draw = function(dx, dy, ctx) {
-		ctx.drawImage(img, x, y, w, h, Math.floor(dx)+ox, Math.floor(dy)+oy, w, h);
+		ctx.drawImage(img, x, y, w, h, Math.floor(dx) + ox, Math.floor(dy) + oy, w, h);
 	};
 }
 
 function InGame() {
-	var context, canvas;
-	var players = [];
-	var tiles = [];
-	var image = new Image();
+	"use strict";
+	var context, canvas,
+		players = [],
+		tiles = [],
+		image = new Image(),
+		images = [],
+
+		powerPills = [19, 28, 51, 60],
+
+		pos = -1,
+		startTime,
+		powerPillActive = false,
+		powerPillStart,
+		score,
+		gameLength,
+		gameOver = false,
+		activated = false;
+
 	image.src = "images/image.png";
-	var images = [];
 	images["Pacman"] = new StaticImage(image, 20, 0, 19, 20);
 	images["Inky"] = new StaticImage(image, 39, 0, 20, 20);
 	images["Blinky"] = new StaticImage(image, 20, 20, 20, 20);
@@ -29,187 +105,102 @@ function InGame() {
 	images["PowerPill"] = new StaticImage(image, 0, 40, 16, 8, 0, 4);
 	images["PowerPillEat"] = new StaticImage(image, 31, 40, 16, 8, 0, 4);
 
-	var powerPills = [19, 28, 51, 60];
-
-	var pos = -1,
-	scoreBoard,
-	startTime,
-	powerPillActive = false,
-	powerPillStart,
-	localPlayerId,
-	score,
-	gameLength,
-	gameOver = false,
-	offsetX = 80,
-	offsetY = 80;
-
 	this.Activate = function() {
-		channel.Connect(this);
-		body.innerHTML = "<canvas id='canvas' width='620' height='620'></canvas><div id='scoreboard'></div>";
-		scoreBoard = document.getElementById('scoreboard');
+		activated = true;
 		canvas = document.getElementById('canvas');
 		context = canvas.getContext('2d');
-		this.Draw();
 		canvas.addEventListener("click", this.OnClick, false);
-		this.GetGameInfo();
-		setInterval(ingame.UpdateScoreBoard, 1000);
-
-	};
-
-	this.GetGameInfo = function() {
-		jx.load("getgameinfo", function(data) {
-			ingame.UpdateGame(data);
-		});
+		setInterval(this.UpdateScoreBoard, 300);
 	};
 
 	this.OnClick = function(e) {
-		var x = e.offsetX;
-		var y = e.offsetY;
-		x = Math.floor((x-offsetX)/16);
-		y = Math.floor((y-offsetY)/16);
-		var tile = -1;
-		if ((x%2 == 0 && y%2 == 0)){
-			if (y%8 == 0) {
-				tile = x/2+(y/8)*16;
-			} else if (x%6 == 0) {
-				tile = 79+y/2+(x/6)*12-Math.floor(y/8);
-			}
-		}
-		if (tile == -1) {
+		var tile = XYToTile(e.offsetX - offset, e.offsetY - offset);
+		if (tile === -1) {
 			return;
 		}
 
-		if (pos == -1 || ingame.IsBeside(tile, pos)) {
+		if (pos === -1 || IsBeside(tile, pos)) {
 			pos = tile;
-			jx.load("moveto?pos="+tile, function(data) {ingame.UpdateGame(data);});
+			websocket.send("moveto;"+tile);
 		}
-	};
-
-	this.TileToXY = function(tile) {
-		var x = 0;
-		var y = 0;
-		if (tile < 80) {
-			x = tile%16;
-			y = ((tile-x)/16)*128+offsetY;
-			x = (x * 32) + offsetX;
-		} else {
-			tile -= 80;
-			y = tile%12;
-			x = ((tile-y)/12)*96+offsetX;
-			y = (y + Math.floor(y/3)+1)*32+offsetY;
-		}
-		var o = {};
-		o.x = x;
-		o.y = y;
-		return o;
-	}
-
-	this.IsBeside = function(a, b) {
-		var oa, ob;
-		oa = this.TileToXY(a);
-		ob = this.TileToXY(b);
-		if (oa.x == ob.x && (oa.y + 32 == ob.y || oa.y - 32 == ob.y)) {
-			return true;
-		} else if (oa.y == ob.y && (oa.x + 32 == ob.x || oa.x - 32 == ob.x)) {
-			return true;
-		}
-		return false;
 	};
 
 	this.MarkTile = function(tile, type) {
-		var o = this.TileToXY(tile);
+		var o = TileToXY(tile);
 		if (type) {
 			images[type].Draw(o.x, o.y, context);
 		}
 	};
 
 	this.UpdateGame = function(data) {
-		console.log(data)
-		var o = JSON.parse(data);
-		if (o.type == "move") {
-			if (o.eat) {
+		console.log(data);
+		if (!activated) {
+			this.Activate();
+		}
+		var o = JSON.parse(data), i;
+		if (o.type === "move") {
+			if (o.role === "Pacman") {
 				tiles.push(o.pos);
 			}
 			this.MarkTile(o.pos, o.role);
-			for (var i = 0; i < players.length; i += 1) {
-				if (players[i].name == o.name) {
+			for (i = 0; i < players.length; i += 1) {
+				if (players[i].role === o.role) {
 					players[i].pos = o.pos;
 				}
 			}
 			powerPillActive = o.powerPillActive;
 			if (powerPillActive) {
-				powerPillStart = Date.parse(o.powerPillStart+" GMT");
+				powerPillStart = o.powerPillStart;
 			}
 			score = o.score;
-		}else if (o.type == "score") {
-			score = o.score
-		} else if (o.type == "full") {
+		} else if (o.type === "score") {
+			score = o.score;
+		} else if (o.type === "full") {
 			players = o.players;
 			tiles = o.tiles;
-			startTime = Date.parse(o.startTime+" GMT");
+			startTime = o.startTime;
 			powerPillActive = o.powerPillActive;
 			if (powerPillActive) {
-				powerPillStart = Date.parse(o.powerPillStart+" GMT");
+				powerPillStart = o.powerPillStart;
 			}
-			for (var i = 0; i < players.length; i += 1) {
+			for (i = 0; i < players.length; i += 1) {
 				this.MarkTile(players[i].pos, players[i].role);
 			}
-			localPlayerId = o.localPlayer;
 			score = o.score;
 			gameLength = o.gamelength;
-			pos = players[localPlayerId].pos;
+
 			this.UpdateScoreBoard();
-		} else if (o.type == "end") {
-			menu.Activate();
 		}
 		this.Draw();
-	};
-
-	this.FormattedMinutesSeconds = function(time) {
-		var seconds = time % 60;
-		var minutes = Math.floor(time / 60);
-		var output = minutes+":";
-		if (seconds < 10) {
-			output += "0";
-		}
-		output += seconds;
-		return output;
 	};
 
 	this.UpdateScoreBoard = function() {
 		if (gameOver) {
 			return;
 		}
-		var output = "";
-		var time = new Date().getTime();
-		if (startTime) {
-			var delta = time - startTime;
-			delta = gameLength *60 - Math.floor(delta/1000);
-			if (delta < 0) {
-				output += "Game Over";
-			} else {
-				output += "Time Left: "+ingame.FormattedMinutesSeconds(delta)+"<br \>";
-			}
-			
+		var time = new Date().getTime(), timetext, delta;
+
+		context.fillStyle = "rgb(0, 0, 0)";
+		context.fillRect(150, 0, 400, 50);
+
+		context.fillStyle = "rgb(255,255,255)";
+		delta = 60 * gameLength - ((time / 1000) - startTime);
+		delta = Math.floor(delta);
+		if (delta < 0) {
+			timetext = "Game Over";
+		} else {
+			timetext = "Time Left: " + FormattedMinutesSeconds(delta);
 		}
+		context.fillText(timetext, 150, 20);
+		
 		if (powerPillActive) {
-			var delta = time - powerPillStart;
-			delta = 2*60 - Math.floor(delta/1000);
+			delta = 120 - ((time / 1000) - powerPillStart);
 			if (delta < 0) {
 				powerPillActive = false;
 			} else {
-				output += "Power Pill Time Left: "+ingame.FormattedMinutesSeconds(delta)+"<br \>";
+				context.fillText("Power Pill Time Left: " + FormattedMinutesSeconds(delta), 300, 20);
 			}
 		}
-		output += "Score: "+score+"<br \>";
-		output += "<input type='submit' onclick='ingame.Eaten();' value='I was eaten'>";
-		scoreBoard.innerHTML = output;
-	};
-
-	this.Eaten = function() {
-		jx.load("eaten", function(data) {
-
-		});
 	};
 
 	this.onMessage = function(data) {
@@ -217,35 +208,35 @@ function InGame() {
 	};
 
 	this.Draw = function() {
-		
+		var i;
 		context.fillStyle = "rgb(0, 0, 0)";
 		context.fillRect(0, 0, 650, 650);
 		context.fillStyle = "rgb(0, 0, 128)";
 
-		for (var i = 0; i < 6; i += 1) {
-			context.fillRect(offsetX+96*i, offsetX, 16, 528);
+		for (i = 0; i < 6; i += 1) {
+			context.fillRect(offset + 96 * i, offset, 16, 528);
 		}
-		for (var i = 0; i < 5; i += 1) {
-			context.fillRect(offsetX, offsetX+128*i, 480, 16);
+		for (i = 0; i < 5; i += 1) {
+			context.fillRect(offset, offset + 128 * i, 480, 16);
 		}
 
 		context.fillStyle = "rgb(255,255,255)";
-		for (var i = 0; i < 152; i += 1) {
-			if (powerPills.indexOf(i) != -1) {
+		for (i = 0; i < 152; i += 1) {
+			if (powerPills.indexOf(i) !== -1) {
 				this.MarkTile(i, "PowerPill");
 			} else {
 				this.MarkTile(i, "Pill");
 			}
 		}
-		for (var i = 0; i < tiles.length; i += 1) {
+		for (i = 0; i < tiles.length; i += 1) {
 			var x = parseInt(tiles[i]);
-			if (powerPills.indexOf(x) != -1) {
+			if (powerPills.indexOf(x) !== -1) {
 				this.MarkTile(x, "PowerPillEat");
 			} else {
 				this.MarkTile(x, "Eat");
 			}
 		}
-		for (var i = 0; i < players.length; i += 1) {
+		for (i = 0; i < players.length; i += 1) {
 			this.MarkTile(players[i].pos, players[i].role);
 		}
 		context.font = "20px sans-serif";
@@ -261,7 +252,10 @@ function InGame() {
 		context.fillText("3", 60, 351);
 		context.fillText("4", 60, 479);
 		context.fillText("5", 60, 607);
+
+		context.fillText("Score: " + score, 10, 20);
+		this.UpdateScoreBoard();
 	};
 }
 
-var ingame = new InGame();
+
